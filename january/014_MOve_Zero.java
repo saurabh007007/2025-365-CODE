f2826c85ee65a4436de2d4083206bbc74eccf321
saurabh007007 <loverboybhol143@gmail.com>
2025-01-13 21:58:35 +0530

    added the move zwero solution

---
+++ january/014_MOve_Zero.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void moveZeroes(int[] arr) {
+        int j=-1;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==0){
+                j=i;
+                break;
+            }
+        }
+        if(j==-1) return;
+        for(int i =j+1;i<arr.length;i++){
+            if(arr[i]!=0){
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+                j++;
+            }
+        }
+    }
+}
