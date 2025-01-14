84fed8fc5183b319ac28f1e5cc15b6721b83a6cf
saurabh007007 <loverboybhol143@gmail.com>
2025-01-14 10:03:56 +0530

    adding the cpp also

---
+++ Codehelp(DSA)/001_Arrays/Array_001.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int n=10;
+    cout<<n;
+    int arr[10];
+    for(int i=0;i<sizeof(arr)/sizeof(arr[0]);i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<sizeof(arr)/sizeof(arr[0]);i++){
+        cout<<arr[i];
+    }
+    return 0;
+}
