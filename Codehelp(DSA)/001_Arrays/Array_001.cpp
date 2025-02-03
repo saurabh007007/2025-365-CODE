e469a43705518801b3fc1434ff08f3408c72cadc
saurabh007007 <loverboybhol143@gmail.com>
2025-02-03 21:42:26 +0530

    arr

--- Codehelp(DSA)/001_Arrays/Array_001.cpp
+++ Codehelp(DSA)/001_Arrays/Array_001.cpp
@@ -3,18 +3,18 @@
 using namespace std;
 
 
-int main(){
-    int n=10;
-    cout<<n;
-    int arr[10];
-    for(int i=0;i<sizeof(arr)/sizeof(arr[0]);i++){
-        cin>>arr[i];
-    }
-    for(int i=0;i<sizeof(arr)/sizeof(arr[0]);i++){
-        cout<<arr[i];
-    }
-    return 0;
-}
+// int main(){
+//     int n=10;
+//     cout<<n;
+//     int arr[10];
+//     for(int i=0;i<sizeof(arr)/sizeof(arr[0]);i++){
+//         cin>>arr[i];
+//     }
+//     for(int i=0;i<sizeof(arr)/sizeof(arr[0]);i++){
+//         cout<<arr[i];
+//     }
+//     return 0;
+// }
 //array that can store 10 bolean
 
 int booleanArray(){
@@ -26,4 +26,21 @@ int booleanArray(){
         cout<<arr[i];
     }
     return 0;
+}
+
+void LinearSearch(int arr[],int size,int k){
+    int n=size;
+    for(int i=0;i<n;i++){
+        if(arr[i]==k){
+            cout<<i;
+        }
+        
+        
+    }
+    
+}
+int main(){
+    int arr[5]={1,3,4,502,2};
+    int k=2;
+    LinearSearch(arr,5,k);
 }
